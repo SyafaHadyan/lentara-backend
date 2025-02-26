4c9185ff9ce16049b7750037ab8bbb2cb8ed08a1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2025-02-26 23:20:08 +0700

    Fix min

--- internal/domain/dto/product.go
+++ internal/domain/dto/product.go
@@ -7,20 +7,20 @@ import (
 )
 
 type RequestCreateProduct struct {
-	Title         string `json:"title" validate:"required,min=5"`
+	Title         string `json:"title" validate:"required,min=3"`
 	Description   string `json:"description" validate:"required,min=5"`
 	Specification string `json:"specification" validate:"required,min=5"`
-	Category      string `json:"category" validate:"required,min=5"`
+	Category      string `json:"category" validate:"required,min=1"`
 	Price         int64  `json:"price" validate:"required"`
 	Stock         int32  `json:"stock" validate:"required"`
 	PhotoUrl      string `json:"photo_url" validate:"required"`
 }
 
 type UpdateProduct struct {
-	Title         string  `json:"title" validate:"omitempty,min=5"`
+	Title         string  `json:"title" validate:"omitempty,min=3"`
 	Description   string  `json:"description" validate:"omitempty,min=5"`
 	Specification string  `json:"specification" validate:"omitempty,min=5"`
-	Category      string  `json:"category" validate:"omitempty,min=5"`
+	Category      string  `json:"category" validate:"omitempty,min=1"`
 	Price         int64   `json:"price"`
 	Stock         int32   `json:"stock"`
 	RentCount     int32   `json:"rent_count"`
