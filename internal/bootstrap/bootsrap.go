101dd388108b1637a1f58e1c9e070871638e95d3
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2025-02-28 20:33:46 +0700

    Fix cors

--- internal/bootstrap/bootsrap.go
+++ internal/bootstrap/bootsrap.go
@@ -10,6 +10,7 @@ import (
 	"lentara-backend/internal/infra/mysql"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func Start() error {
@@ -36,6 +37,13 @@ func Start() error {
 	val := validator.New()
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowHeaders: "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		AllowOrigins: "*",
+		// AllowCredentials: true,
+		AllowMethods: "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+	}))
 
 	v1 := app.Group("/api/v1")
 
